5d960478efd8634bad588975e4cc91974cd02af7
cody <qudgus1984@naver.com>
2023-02-20 15:45:29 +0900

    [Design] MainMemo UI 구성 완료

---
+++ TellMeWhatToDo/MainListView.swift
@@ -0,0 +1,37 @@
+//
+//  MainListView.swift
+//  TellMeWhatToDo
+//
+//  Created by 이병현 on 2023/02/20.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    @EnvironmentObject var store: MemoStore
+    
+    var body: some View {
+        NavigationView {
+            List(store.list) { memo in
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1)
+                    Text(memo.insertDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 메모")
+        }
+        
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+            .environmentObject(MemoStore())
+    }
+}
--- TellMeWhatToDo/TellMeWhatToDoApp.swift
+++ TellMeWhatToDo/TellMeWhatToDoApp.swift
@@ -13,7 +13,7 @@ struct TellMeWhatToDoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
