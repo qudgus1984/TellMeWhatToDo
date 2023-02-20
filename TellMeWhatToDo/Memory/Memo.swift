0df5ef66320070179344c5c176068ab06128a3ba
cody <qudgus1984@naver.com>
2023-02-20 15:17:05 +0900

    [Add] memo data set insert / delete func add

--- TellMeWhatToDo/Memory/Memo.swift
+++ TellMeWhatToDo/Memory/Memo.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject { // Data를 쉽게 바인딩, 메모를 자동 편집 및 업데이트할 때 필요한 것
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(content: String, insertDate: Date = Date.now) {
+        id = UUID()
+        self.content = content
+        self.insertDate = insertDate
+    }
+}
